0eca2d47488c5ec26a42471f1589d4f4ba775a65
gravetir <dimadt505228@gmail.com>
2023-09-06 09:27:10 +0300

    lecture_1 was added

---
+++ Python/1.py
@@ -0,0 +1,16 @@
+# i:int
+# f: float=.0
+# s: str='sdas'
+# b: bool= True
+# n: None =None
+_list1: list = [1, 3, 23, 45, False, True, "sdads"]
+_list2: list = [1, 2, 3, 4, 5]
+_set1: set = {1, 2, 3, 4, 5}
+_set2: set = {1, 2, 3, 4, 5}
+_set3: set = {"m", "s", "s", "i", "s", "s", "i", "p", "p", "i"}
+_set4: set = {"n", "t", "t", "r"}
+_set4_list = list(_set4)
+# print(_set3)
+# print(_set4)
+# print(_set4_list[0])
+# print(_set3.intersection(_set4))
